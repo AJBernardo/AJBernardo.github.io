1f4f3f6c12e4e9b00e7290ecab3bcbedf349fe34
AJBernardo <austinebernardo8104@gmail.com>
2025-04-11 03:15:50 +0800

    [FE] - Implemented structure

--- ADET/A02/index.php
+++ ADET/A02/index.php
@@ -10,20 +10,29 @@
     <link href="assets/img/CafeLycoReco_Logo.svg" rel="icon">
     <style>
         .navbar {
-            background: linear-gradient(90deg, #FFF8F0 -8.14%, #F8C8DC 100%);
-            opacity: 0;
-            transition: opacity 0.3s ease, visibility 0.3s ease;
+            background-image: url(assets/img/navBg.gif);
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: top center;
         }
 
         .main-logo {
             max-height: 60px;
+        }
+
+        .main-container{
+            background: linear-gradient(310deg, #F9F1F0 15.23%, #FADCD9 33.31%);
+        }
+
+        .card {
+            height: calc(100vh - 134px);
         }
     </style>
 </head>
 
 <body>
     <div>
-        <nav class="navbar bg-navbar fixed-top">
+        <nav class="navbar bg-navbar">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">
                     <img src="assets/img/CafeLycoReco_Wordmark.png" class="main-logo img-fluid">
@@ -31,14 +40,26 @@
             </div>
         </nav>
 
-        <div class="container" style="height: 1000px;">
+        <div class="container-fluid">
+            <div class=" main-container row">
+                <div class="col-12 col-md-3 p-4">
+                    <div class="card rounded-4 shadow">
+
+                    </div>
+                </div>
+                <div class=" col-12 col-md-9 py-4 pe-4 ps-4 ps-md-0">
+                    <div class="card rounded-4 shadow">
+
+                    </div>
+                </div>
+            </div>
 
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq"
         crossorigin="anonymous"></script>
-    <script>
+    <!-- <script>
         document.addEventListener("scroll", () => {
             const navbar = document.querySelector(".navbar");
             if (window.scrollY > 100) {
@@ -49,7 +70,7 @@
                 navbar.style.visibility = "hidden";
             }
         });
-    </script>
+    </script> -->
 </body>
 
 </html>
