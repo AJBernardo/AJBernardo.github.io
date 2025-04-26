48b43dca43fee7fc7e24e308de8f4542daa25e97
AJBernardo <austinebernardo8104@gmail.com>
2025-04-26 12:05:08 +0800

    [FE] - modularize the navbar

---
+++ ADET/A03/assets/js/navbar.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("navbar-container").innerHTML = 
+    `<nav class="navbar navbar-expand-lg p-0 fixed-top">
+        <div class="container-fluid d-flex justify-content-between">
+            <a class="navbar-brand" href="#"><img src="assets/img/dedsecLogo.webp" alt="DedSec" class="img-fluid"
+                    style="max-height: 50px;"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="index.html">HOME</a>
+                    <a class="nav-link" href="council.html">THE COUNCIL</a>
+                    <a class="nav-link" href="#">ABOUT</a>
+                    <a class="nav-link" href="#">WATCHDOGS</a>
+                </div>
+            </div>
+        </div>
+    </nav>`;
+})
+
+function fadeInOutNavbar() {
+    const navbar = document.querySelector('.navbar');
+
+    if (window.innerWidth > 576) {
+        if (window.scrollY < 695) {
+            navbar.style.backgroundColor = 'transparent';
+        } else {
+            navbar.style.backgroundColor = 'var(--primaryBackgroundColor)';
+        }
+    } else {
+        navbar.style.backgroundColor = 'var(--primaryBackgroundColor)';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {    
+    fadeInOutNavbar();
+    window.addEventListener('scroll', fadeInOutNavbar);    
+    window.addEventListener('resize', () => { 
+        fadeInOutNavbar();
+    });
+});
