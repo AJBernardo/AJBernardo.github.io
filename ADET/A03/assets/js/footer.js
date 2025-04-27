8840210bb4de3e3bdb44dead9c6708f13ac44ddd
AJBernardo <austinebernardo8104@gmail.com>
2025-04-27 12:40:46 +0800

    [FE] - Modularize the footer section

---
+++ ADET/A03/assets/js/footer.js
@@ -0,0 +1,21 @@
+document.getElementById('footer-container').innerHTML = 
+` <div class="container">
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 mt-4 border-top text-black">
+            <p class="col-md-4 mb-0 text-white">&copy; 2025 DEDSEC</p>
+
+            <a href="./"
+                class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+                aria-label="DedSec">
+                <img src="assets/img/dedsecLogo.webp" alt="DedSec" class="img-fluid"
+                style="max-height: 50px;">
+            </a>
+
+            <ul class="nav col-md-4 justify-content-end">
+                <li class="nav-item"><a href="#" class="text-decoration-none px-3 text-white">Home</a></li>
+                <li class="nav-item"><a href="#" class="text-decoration-none px-3 text-white">The Council</a></li>
+                <li class="nav-item"><a href="#" class="text-decoration-none px-3 text-white">About</a></li>
+                <li class="nav-item"><a href="#" class="text-decoration-none px-3 text-white">Thy Austine</a></li>
+            </ul>
+        </footer>
+    </div>
+`;
