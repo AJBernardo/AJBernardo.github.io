c3f25f3acb2c88b36f185f26b3077f4f0abc908c
AJBernardo <austinebernardo8104@gmail.com>
2025-04-28 04:43:26 +0800

    [FE] - Fixed responsiveness of the footer

--- ADET/A03/assets/js/footer.js
+++ ADET/A03/assets/js/footer.js
@@ -1,19 +1,19 @@
 document.getElementById('footer-container').innerHTML =
     ` <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 text-black">
-            <p class="col-md-4 mb-0 text-white">&copy; 2025 DEDSEC</p>
+            <p class="col-sm-4 col-md-5 col-lg-4 mb-0 text-white dedsec-copy">&copy; 2025 DEDSEC</p>
 
             <a href="./"
-                class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+                class="col-12 col-md-2 col-lg-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none dedsec-footer"
                 aria-label="DedSec">
                 <img src="assets/img/dedsecLogo.webp" alt="DedSec" class="img-fluid"
                 style="max-height: 50px;">
             </a>
 
-            <ul class="nav col-md-4 justify-content-end">
-                <li class="nav-item"><a href="home.html" class="text-decoration-none px-3 text-white">Home</a></li>
-                <li class="nav-item"><a href="council.html" class="text-decoration-none px-3 text-white">The Council</a></li>
-                <li class="nav-item"><a href="about.html" class="text-decoration-none px-3 text-white">About</a></li>
-                <li class="nav-item"><a href="/" class="text-decoration-none px-3 text-white">Thy Austine</a></li>
+            <ul class="nav col-sm-8 col-md-5 col-lg-4 justify-content-end dedsec-links">
+                <li class="nav-item"><a href="home.html" class="text-decoration-none px-2 px-sm-2 pe-md-3 ps-md-0 px-lg-2 px-xxl-3 text-white">Home</a></li>
+                <li class="nav-item"><a href="council.html" class="text-decoration-none px-2 px-sm-2 pe-md-3 ps-md-0 px-lg-2 px-xxl-3 text-white">The Council</a></li>
+                <li class="nav-item"><a href="about.html" class="text-decoration-none px-2 px-sm-2 pe-md-3 ps-md-0 px-lg-2 px-xxl-3 text-white">About</a></li>
+                <li class="nav-item"><a href="/" class="text-decoration-none ps-md-0 px-2 px-sm-2 px-lg-2 px-xxl-3 text-white">Thy Austine</a></li>
             </ul>
         </footer>
     `;
