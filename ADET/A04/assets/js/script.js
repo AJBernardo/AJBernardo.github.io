4ca4ce10ed2cf0849f9805e38b92faed1ea1f91e
AJBernardo <austinebernardo8104@gmail.com>
2025-05-04 01:42:50 +0800

    [FE] - Implemented color modes

--- ADET/A04/assets/js/script.js
+++ ADET/A04/assets/js/script.js
@@ -1,4 +1,6 @@
 const salesList = document.getElementById("sales-list");
+const categoryList = document.getElementById('category-list');
+const menuItemContainer = document.getElementById('menu-item-container');
 const totalAmount = document.getElementById("total-amount");
 const btnCancel = document.getElementById('button-cancel');
 const btnPay = document.getElementById('button-pay');
@@ -8,16 +10,15 @@ btnCancel.disabled = true;
 btnPay.disabled = true;
 
 function displayCategoryList() {
-    const categoryList = document.getElementById('category-list');
-
     menuList.forEach((menu, i) => {
         categoryList.innerHTML +=
             `<button class="btn btn-categories btn-outline-light" onclick="displayMenuItems(${i});">${menu.category.toUpperCase()}</button>`;
     })
+
+    displayMenuItems(0);
 }
 
 function displayMenuItems(i) {
-    const menuItemContainer = document.getElementById('menu-item-container');
     menuItemContainer.innerHTML = "";
 
     menuList[i].items.forEach((item, itemIndex) => {
@@ -26,15 +27,15 @@ function displayMenuItems(i) {
             <img src="assets/img/${item.itemPicture}.webp" alt="${item.itemName}" class="img-fluid item-pic m-3">
             <h6 class="heading text-center">${item.itemName.toUpperCase()}</h6>
             <div class="d-flex gap-3 align-items-end my-4">
-                <button style="background-color: transparent; height: 20px;" id="${item.itemCode}-subtract"
+                <button class="btn btn-light d-flex align-items-center" style="height: 30px; border: 2px solid #212529" id="${item.itemCode}-subtract"
                     onclick="removeItem(${i}, ${itemIndex});" ${item.quantityOrdered === 0 ? "disabled" : ""}>-</button>
                 <h6 id="${item.itemCode}-quantity">${item.quantityOrdered}</h6>
-                <button style="background-color: transparent; height: 20px;" id="${item.itemCode}-add"
+                <button class="btn btn-light d-flex align-items-center" style="height: 30px; border: 2px solid #212529" id="${item.itemCode}-add"
                     onclick="addItem(${i}, ${itemIndex});">+</button>
             </div>
         </div>`;
     })
-    console.log(menuList[0].items[0].quantityOrdered);
+
 }
 
 function addItem(i, itemIndex) {
@@ -51,15 +52,15 @@ function addItem(i, itemIndex) {
     total += itemArray.itemPrice;
     isReceiptEmpty();
 
-    if(itemExist){
+    if (itemExist) {
         const salesQuantity = document.getElementById(`${itemArray.itemCode}-qty`);
         const salesSubTotal = document.getElementById(`${itemArray.itemCode}-subtotal`);
-        
+
         salesQuantity.textContent = `${itemArray.quantityOrdered} × ${itemArray.itemPrice}.00`;
         salesSubTotal.textContent = `₱${subTotal}.00`;
-    }else{
-        salesList.innerHTML += 
-        `<div class="mb-3" id="${itemArray.itemCode}-receipt">
+    } else {
+        salesList.innerHTML +=
+            `<div class="mb-3" id="${itemArray.itemCode}-receipt">
             <div class="d-flex justify-content-between">
                     <h6>${itemArray.itemName}</h6><span class="text-end" id="${itemArray.itemCode}-subtotal">₱${itemArray.itemPrice}.00</span>
             </div>
@@ -85,12 +86,12 @@ function removeItem(i, itemIndex) {
         const subTotal = (itemArray.itemPrice * itemArray.quantityOrdered);
         total -= itemArray.itemPrice
         isReceiptEmpty();
-        if(salesQuantity){
+        if (salesQuantity) {
             salesQuantity.textContent = `${itemArray.quantityOrdered} × ${itemArray.itemPrice}.00`;
             salesSubTotal.textContent = `₱${subTotal}.00`;
             totalAmount.textContent = `₱${total}.00`
         }
-        
+
         if (itemArray.quantityOrdered === 0) {
             const itemExist = document.getElementById(`${itemArray.itemCode}-receipt`);
 
@@ -106,34 +107,68 @@ function removeItem(i, itemIndex) {
     isReceiptEmpty();
 }
 
-function isReceiptEmpty(){
-    if(total > 0){
+function isReceiptEmpty() {
+    if (total > 0) {
         btnCancel.disabled = false;
         btnPay.disabled = false;
-    }else{
+    } else {
         btnCancel.disabled = true;
         btnPay.disabled = true;
     }
 }
 
-function reset(){
+function reset() {
     total = 0;
     salesList.innerHTML = '';
     totalAmount.innerHTML = '';
 
-    menuList.forEach(menu =>{
-        menu.items.forEach(item =>{
+    menuList.forEach(menu => {
+        menu.items.forEach(item => {
             item.quantityOrdered = 0;
-            
+
             const itemQuantity = document.getElementById(`${item.itemCode}-quantity`);
             const itemBtnSubtract = document.getElementById(`${item.itemCode}-subtract`);
-            if(itemQuantity){
+            if (itemQuantity) {
                 itemQuantity.innerHTML = 0;
                 itemBtnSubtract.disabled = true;
             }
         })
     })
+
+    isReceiptEmpty();
 }
 
+function changeMode() {
+    const toggleColor = document.getElementById('toggle-color');
+    const logo = document.getElementById('logo');
+    const body = document.body;
 
+    toggleColor.addEventListener('click', () => {
+        const currentTheme = body.getAttribute('data-bs-theme');
+
+        if (currentTheme === 'dark') {
+            body.setAttribute('data-bs-theme', 'light');
+            toggleColor.innerHTML = 'DARK MODE';
+            logo.src = 'assets/img/nami_light.png';
+        } else {
+            body.setAttribute('data-bs-theme', 'dark');
+            toggleColor.innerHTML = 'LIGHT MODE';
+            logo.src = 'assets/img/nami_dark.png';
+        }
+
+        const buttons = document.getElementsByClassName('btn');
+        //FOR FUTURE REFERENCE. Array.from() was used to convert buttons(elements) to an array
+        Array.from(buttons).forEach(btn => {
+            if (btn.classList.contains('btn-outline-light')) {
+                btn.classList.remove('btn-outline-light');
+                btn.classList.add('btn-dark');
+            } else if (btn.classList.contains('btn-dark')) {
+                btn.classList.remove('btn-dark');
+                btn.classList.add('btn-outline-light');
+            }
+        });
+    });
+}
+
+changeMode();
 displayCategoryList();
